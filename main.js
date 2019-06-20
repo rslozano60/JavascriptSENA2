a629c6bdfbee36e37ac2961d20aa65ed7c883b55
rslozano60 <rslozano60@misena.edu.co>
2019-06-20 08:10:19 -0500

    third commit

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-alert("ESTAMOS EMPEZANDO A USAR GIT")
-
-console.log("AHORA VAMOS A SUBIR NUESTROS ARCHIVOS A GIT HUB")
+//alert("ESTAMOS EMPEZANDO A USAR GIT")
+//console.log("AHORA VAMOS A SUBIR NUESTROS ARCHIVOS A GIT HUB")
+var persona= {
+	nombre: "Axel",
+	apellido:"Franco",
+	cedula:"1234567895",
+	 edad: 18
+}
+if(persona.edad >= 18)
+{
+alert(persona.nombre+" "+"es mayor edad y tiene "+ persona.edad )
+}
