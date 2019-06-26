5abbe863e61c1af9a1596d6b3adb2ef41084071c
rslozano60 <rslozano60@misena.edu.co>
2019-06-26 11:12:42 -0500

    Arreglo

--- main.js
+++ main.js
@@ -1,12 +1,125 @@
 //alert("ESTAMOS EMPEZANDO A USAR GIT")
 //console.log("AHORA VAMOS A SUBIR NUESTROS ARCHIVOS A GIT HUB")
-var persona= {
+var personas= [{
 	nombre: "Axel",
 	apellido:"Franco",
 	cedula:"1234567895",
-	 edad: 18
-}
-if(persona.edad >= 18)
+	edad: 19,
+	peliculafavorita:"Crespusculo",
+	generopelicula:"Romance Juvenil"
+},
 {
-alert(persona.nombre+" "+"es mayor edad y tiene "+ persona.edad )
-}
+	nombre: "Fana",
+	apellido:"Fontana",
+	cedula:"1234567895",
+	 edad: 30,
+	 peliculafavorita:"Duro de matar 1",
+	 generopelicula:"Accion"
+},
+{
+	nombre: "Maria",
+	apellido:"Posada",
+	cedula:"1234567895",
+	 edad: 8,
+	 peliculafavorita:"Buscando a Nemo",
+	 generopelicula:"Infantil"
+
+}]
+    console.log(personas)
+
+	for (var i = 0; i < personas.length; i++) {
+		alert("Hola "+ 
+				personas[i].nombre+" "+
+				personas[i].apellido+" de "+
+				personas[i].edad+" años")
+		if(personas[i].edad > 18)
+		{	
+			
+			switch (personas[i].generopelicula) {
+			  case 'Accion':
+			    alert("Sus 3 peliculas recomendads son "+ 
+			    	"Duro de matar 2"+
+			    	" Rambo"+
+			    	" John Wick")
+			    break;
+			  case 'Romance Juvenil':
+			    alert("Sus 3 peliculas recomendads son "+ 
+			    	"Bajo la misma estrella"+
+			    	" Amanecer"+
+			    	" Pueblito de papel")
+			    break;
+			  case 'Aventura':
+			    alert("Sus 3 peliculas recomendads son "+ 
+			    	" La isla del tesoro"+
+			    	" Las aventuras de tintin"+
+			    	" El viaje al centro de la tierra")
+			    break;
+			  case 'Comedia':
+			    alert("Sus 3 peliculas recomendads son "+ 
+			    	" Deadpool"+
+			    	" La mascara"+
+			    	" Zoolander")
+			    break;
+			  case 'Drama':
+			  case 'Suspenso':
+			    alert("Sus 3 peliculas recomendads son "+ 
+			    	"El abogado del mal"+
+			    	" 8 minutos antes de morir"+
+			    	" la forma del h2o")
+			    break;
+			  default:
+			    alert('Lo lamentamos, por el momento no sabemos de ' + personas[i].generopelicula + '.');
+			}
+		}else if(personas[i].edad >= 14 ){
+			switch (personas[i].generopelicula) {
+			  case 'Accion':
+			    alert("Sus 3 peliculas recomendadas son "+ 
+			    	" Como entrenar a tu dragon"+
+			    	" Las cronicas de narnia"+
+			    	" Hombres de negro")
+			    break;
+			  case 'Romance Juvenil':
+			    alert("Sus 3 peliculas recomendadas son "+ 
+			    	"Bajo la misma estrella"+
+			    	" Amanecer"+
+			    	" Orgullo y prejuicio")
+			    break;
+			  case 'Aventura':
+			    alert("Sus 3 peliculas recomendadas son "+ 
+			    	" el libro de la selva"+
+			    	" Las aventuras de tintin"+
+			    	" El viaje al centro de la tierra")
+			    break;
+			  case 'Comedia':
+			    alert("Sus 3 peliculas recomendadas son "+ 
+			    	" Spiderman lejos de casa"+
+			    	" Toy story 4"+
+			    	" Ace ventura")
+			    break;
+			  case 'Drama':
+			  case 'Suspenso':
+			    alert("Sus 3 peliculas recomendadas son "+ 
+			    	"El rey leon"+
+			    	" El gran showman"+
+			    	" Siempre a tu lado")
+			    break;
+			  default:
+			    alert('Lo lamentamos, por el momento no sabemos de ' + personas[i].generopelicula + '.');
+			}
+		}else{
+			switch (personas[i].generopelicula) {
+			  case 'Infantil':
+			    alert("Sus 3 peliculas recomendadas son "+ 
+			    	" Dumbo"+
+			    	" Coco"+
+			    	" Moana")
+			    break;
+			}
+
+		}
+	}
+
+
+
+
+
